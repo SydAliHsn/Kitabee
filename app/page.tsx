66d30bb51c02ce7200c9878aeb86d59a36d46547
Syed Ali Hassan <syedalihassan6651@gmail.com>
2024-05-21 18:32:16 +0500

    Minor styling changes

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,10 @@ export default async function Page() {
     <>
       <section>
         <div className="flex flex-col items-center justify-center py-12">
-          <h1 className="pb-6 text-center text-3xl font-bold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          <h1 className="text-center text-3xl font-bold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Exclusive Notes PDFs for Free!
           </h1>
-          <p className="mt-2 max-w-4xl text-center text-lg leading-8 text-gray-600 dark:text-gray-400 sm:text-left xl:text-xl">
+          <p className="mt-6 max-w-4xl text-center text-lg leading-7 text-gray-600 dark:text-gray-400 sm:mt-8 sm:text-left sm:leading-8 xl:text-xl">
             Kitabee is a community initiative to give students access to School & College Notes from
             popular publishers without purchasing them. Students are requested to contribute to the
             Notes collection by creating PDFs of the notes they own and uploading{' '}
@@ -26,7 +26,7 @@ export default async function Page() {
             . Enjoy and Study well!
           </p>
 
-          <div className="mt-10 flex flex-wrap justify-center gap-8 sm:gap-10">
+          <div className="mt-8 flex flex-wrap justify-center gap-8 sm:mt-10 sm:gap-10">
             <Link
               href="/upload"
               className="rounded-md border border-transparent bg-primary-500 px-3 py-2 font-bold text-white shadow-sm hover:bg-primary-600 dark:hover:bg-primary-400 sm:px-4 sm:py-3 sm:text-lg"
